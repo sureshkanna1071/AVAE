b7241e905e1e314ed47cd007c87250ed9efaa07d
sureshkanna1071 <sureshkanna.p1071@gmail.com>
2023-11-23 14:00:09 +0530

    Change logo

--- src/components/pages/success/Success.js
+++ src/components/pages/success/Success.js
@@ -21,7 +21,7 @@ const Success = () => {
       }}
     >
        <Lottie animationData={AnimationData} style={{width: "50vw"}}  />
-       <p className='order_success-message'>Thank you for your order with AVAE. We're processing your order with care, and you can expect an email or sms with all the order details soon. If you have any questions or need assistance, please feel free to reach out to our customer support team at <a href='mailto:matha.agenc@gmail.com'>matha.agenc@gmail.com</a> or call us at <a href="tel:8098845333">918098845333</a>. We appreciate your patience and business!</p>
+       <p className='order_success-message'>Thank you for your order with AVAE. We're processing your order with care, and you can expect an email or sms with all the order details soon. If you have any questions or need assistance, please feel free to reach out to our customer support team at <a href='mailto:matha.agenc@gmail.com'>matha.agenc@gmail.com</a> or call us at <a href="tel:8098845333">918098845333</a>. To cancel your order, kindly reach out to us prior to shipment.</p>
        <Button variant='contained' color='success' onClick={() => navigate("/")}>Go To Homepage</Button>
     </div>
   )

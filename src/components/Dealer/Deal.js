24a93ee006b0895af4e7a78fc92f508fbe569590
sureshkanna1071 <sureshkanna.p1071@gmail.com>
2023-11-23 15:47:10 +0530

    change font size

--- src/components/Dealer/Deal.js
+++ src/components/Dealer/Deal.js
@@ -12,7 +12,7 @@ const Deal = () => {
       {/* <img src={Flash} alt='dealers' /> */}
       <div className='deal-content'>
         <h1>Unlock the Power of Partnership in Oman!</h1>
-        <p style={{fontSize: 'large'}}>AVAE, a renowned name in the world of LED TVs, is proud to announce its expedition into Oman, seeking dynamic partnerships with Dealers and Distributors. As we embark on this exciting journey, we invite you to join hands with us and be part of our success story.</p>
+        <p>AVAE, a renowned name in the world of LED TVs, is proud to announce its expedition into Oman, seeking dynamic partnerships with Dealers and Distributors. As we embark on this exciting journey, we invite you to join hands with us and be part of our success story.</p>
         <Button
           className='moving-content'
           variant="outlined"
